a5c09dbeeb2a1c64bf00102583fbeeb6ce2f53f5
James <jamespatten1996@gmail.com>
2024-06-01 23:09:25 +0100

    removed react imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Connector } from "./Screens/Connector";
 import { OpenAPI } from "api";
