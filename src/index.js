087ab2c167e19f5a2036774957b48ca3e1d73567
James <jamespatten1996@gmail.com>
2024-08-03 14:25:51 +0100

    removed posthog js

--- src/index.js
+++ src/index.js
@@ -2,21 +2,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import * as serviceWorker from "./serviceWorker";
-import { PostHogProvider } from "posthog-js/react";
-
-const options = {
-  api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
-};
 
 const root = createRoot(document.getElementById("root"));
-root.render(
-  <PostHogProvider
-    apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY}
-    options={options}
-  >
-    <App />
-  </PostHogProvider>
-);
+root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
