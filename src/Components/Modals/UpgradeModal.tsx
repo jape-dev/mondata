55d713480f5c38d31029c0f46c43ce0bf4b7b00b
James <jamespatten1996@gmail.com>
2024-09-12 09:51:43 +0100

    updated upgrade link

--- src/Components/Modals/UpgradeModal.tsx
+++ src/Components/Modals/UpgradeModal.tsx
@@ -3,6 +3,8 @@ import "monday-ui-react-core/dist/main.css";
 import { Modal, ModalContent, Button } from "monday-ui-react-core";
 import { Night } from "monday-ui-react-core/icons";
 import { getImageUrl } from "Utils/image";
+import mondaySdk from "monday-sdk-js";
+import { useEffect, useState } from "react";
 
 export interface ModalProps {
   title: string;
@@ -11,14 +13,31 @@ export interface ModalProps {
   setShowModal: (showModal: boolean) => void;
 }
 
+const monday = mondaySdk();
+
 export const UpgradeModal: React.FC<ModalProps> = ({
   title,
   text,
   showModal,
   setShowModal,
 }) => {
+
+  const [accountSlug, setAccountSlug] = useState<string>();
+
+  useEffect(() => {
+    monday.get("location").then((res: any) => {
+      if (res.data && res.data.href) {
+        const url = new URL(res.data.href);
+        const hostname = url.hostname;
+        const parts = hostname.split('.');
+        if (parts.length > 2) {
+          setAccountSlug(`${parts[0]}.monday.com/marketplace/v2/app/installed/10150307`);
+        }
+      }
+    });
+  }, []);
   const handleClick = () => {
-    const link = "https://monday.com/marketplace/listing/10000557/data-importer";
+    const link = accountSlug ? `https://${accountSlug}` : "https://monday.com/marketplace/listing/10000557/data-importer";
     window.open(link, "_blank");
     setShowModal(false);
   };
