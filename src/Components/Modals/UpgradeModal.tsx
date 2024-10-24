9c6ff3296232de69d1f9dd90eb564641e7e6cb2a
James <jamespatten1996@gmail.com>
2024-10-24 17:02:24 +0100

    removed app id from installed link

--- src/Components/Modals/UpgradeModal.tsx
+++ src/Components/Modals/UpgradeModal.tsx
@@ -21,7 +21,6 @@ export const UpgradeModal: React.FC<ModalProps> = ({
   showModal,
   setShowModal,
 }) => {
-
   const [accountSlug, setAccountSlug] = useState<string>();
 
   useEffect(() => {
@@ -29,15 +28,17 @@ export const UpgradeModal: React.FC<ModalProps> = ({
       if (res.data && res.data.href) {
         const url = new URL(res.data.href);
         const hostname = url.hostname;
-        const parts = hostname.split('.');
+        const parts = hostname.split(".");
         if (parts.length > 2) {
-          setAccountSlug(`${parts[0]}.monday.com/marketplace/v2/app/installed/10150307`);
+          setAccountSlug(`${parts[0]}.monday.com/marketplace/v2/app/installed`);
         }
       }
     });
   }, []);
   const handleClick = () => {
-    const link = accountSlug ? `https://${accountSlug}` : "https://monday.com/marketplace/listing/10000557/data-importer";
+    const link = accountSlug
+      ? `https://${accountSlug}`
+      : "https://monday.com/marketplace/listing/10000557/data-importer";
     window.open(link, "_blank");
     setShowModal(false);
   };
@@ -47,8 +48,12 @@ export const UpgradeModal: React.FC<ModalProps> = ({
       <Modal title={title} onClose={() => setShowModal(false)} show={showModal}>
         <ModalContent className="mt-4">
           <p className="mb-5">{text}</p>
-          <img src={getImageUrl("schedule")}  className="mb-5" />
-          <Button leftIcon={Night} onClick={handleClick} color={Button.colors.POSITIVE}>
+          <img src={getImageUrl("schedule")} className="mb-5" />
+          <Button
+            leftIcon={Night}
+            onClick={handleClick}
+            color={Button.colors.POSITIVE}
+          >
             Upgrade
           </Button>
         </ModalContent>
